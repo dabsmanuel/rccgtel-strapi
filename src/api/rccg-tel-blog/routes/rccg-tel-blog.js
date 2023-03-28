e943a8dcc74f68ed468a5d4daf33101be8f8d81d
dabsmanuel <dabsmanuel5015@gmail.com>
2023-03-28 10:11:30 +0100

    first commit

---
+++ src/api/rccg-tel-blog/routes/rccg-tel-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rccg-tel-blog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rccg-tel-blog.rccg-tel-blog');
---
+++ src/api/rccg-tel-blog/services/rccg-tel-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rccg-tel-blog service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rccg-tel-blog.rccg-tel-blog');
